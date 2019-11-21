b36fdf6dc635ab30cb2ba8fc9755683a5abe30d9
Rafael Martinez Estevez <rafael.martinez@upr.edu.cu>
2019-11-21 10:04:45 -0500

    some

--- projects/sceiba-catalog/src/env.js
+++ projects/sceiba-catalog/src/env.js
@@ -2,8 +2,8 @@
     window.__env = window.__env || {};
 
     // API url
-    window.__env.sceibaApi = "https://localhost:5000/api";
-    window.__env.sceibaHost = "https://localhost:5000";
+    window.__env.sceibaApi = "https://10.2.83.157:5000/api";
+    window.__env.sceibaHost = "https://10.2.83.157:5000";
 
     // window.__env.sceibaApi = "https://sceiba-lab.upr.edu.cu/api";
     // window.__env.sceibaHost = "https://sceiba-lab.upr.edu.cu";
