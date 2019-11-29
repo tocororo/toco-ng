23ece009e1a46395fa491e5a259295f22c4a1a09
Rafael Martinez Estevez <rafael.martinez@upr.edu.cu>
2019-11-29 16:35:51 -0500

    taxonomy almost done

--- src/app/taxonomy/taxonomy.service.ts
+++ src/app/taxonomy/taxonomy.service.ts
@@ -1,16 +1,15 @@
-import { Injectable, EventEmitter, Output } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable, Observer, from, PartialObserver} from 'rxjs';
+import {Observable, PartialObserver, Subject} from 'rxjs';
 
 import { EnvService } from '@tocoenv/env.service';
 import { Response } from '@toco/entities/response';
 import { FormSuscriberInterface } from '@toco/forms/forms.service';
-import { response } from 'express';
+import { Vocabulary } from '@toco/entities/taxonomy.entity';
+
 
 @Injectable()
 export class TaxonomyService implements FormSuscriberInterface{
-
-  @Output() vocabularyAdded: EventEmitter<Response<any>> = new EventEmitter();
 
   private httpOptions = {
     headers: new HttpHeaders(
@@ -21,9 +20,22 @@ export class TaxonomyService implements FormSuscriberInterface{
   };
   token = '';
 
-  private sendDataObserver: PartialObserver<Response<any>> = {
+
+  private currentVocabChangedSource = new Subject<Vocabulary>();
+  currentVocabObservable = this.currentVocabChangedSource.asObservable();
+  vocabChanged(vocab: Vocabulary) {
+    this.currentVocabChangedSource.next(vocab);
+  }
+
+  private vocabularyAddedSource = new Subject<Response<any>>();
+  vocabularyAddedObservable = this.vocabularyAddedSource.asObservable();
+  vocabularyAdded(resp: Response<any>) {
+    this.vocabularyAddedSource.next(resp);
+  }
+
+  private addDataObserver: PartialObserver<Response<any>> = {
     next: (resp: Response<any>) => {
-      this.vocabularyAdded.emit(resp);
+      this.vocabularyAdded(resp);
     },
 
     error: (err: any) => {
@@ -35,20 +47,24 @@ export class TaxonomyService implements FormSuscriberInterface{
     }
   };
 
-  constructor(private env: EnvService, private http: HttpClient) { }
-
   addData(data: any): void{
     console.log(data);
     this.httpOptions.headers = this.httpOptions.headers.set('Authorization', 'Bearer ' + this.token);
 
     // tslint:disable-next-line: max-line-length
-    const resp = this.http.post<Response<any>>( this.env.sceibaApi + '/vocabulary/new', data, this.httpOptions ).pipe().subscribe(this.sendDataObserver);
-    // console.log(resp);
+    this.http.post<Response<any>>( this.env.sceibaApi + '/vocabulary/new', data, this.httpOptions ).pipe().subscribe(this.addDataObserver);
   }
+
+  constructor(private env: EnvService, private http: HttpClient) { }
 
 
   getVocabularies():Observable<Response<any>>{
     let req = this.env.sceibaApi + '/vocabularies';
+    return this.http.get<Response<any>>(req);
+  }
+
+  getTermsTreeByVocab(name: string):Observable<Response<any>>{
+    let req = this.env.sceibaApi + '/terms/'+name+'/tree';
     return this.http.get<Response<any>>(req);
   }
 }
--- src/app/taxonomy/taxonomy/taxonomy.component.ts
+++ src/app/taxonomy/taxonomy/taxonomy.component.ts
@@ -1,44 +1,44 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Vocabulary } from '@toco/entities/taxonomy.entity';
+import { TaxonomyService } from '../taxonomy.service';
+import { Subscription, PartialObserver } from 'rxjs';
 
 @Component({
   selector: 'toco-taxonomy',
   templateUrl: './taxonomy.component.html',
   styleUrls: ['./taxonomy.component.scss']
 })
-export class TaxonomyComponent {
+export class TaxonomyComponent implements OnInit, OnDestroy{
 
-  vocabs = { title: 'Vocabularies', cols: 1, rows: 1 };
-  /** Based on the screen size, switch from standard to one column per row */
-  // vocabs = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-  //   map(({ matches }) => {
-  //     if (matches) {
-  //       return [
-  //         { title: 'Vocabularies', cols: 1, rows: 1 }
-  //       ];
-  //     }
+  vocabs = { title: 'Vocabularios', cols: 1, rows: 1 };
+  terms = { title: 'Terminos de', cols: 1, rows: 2 };
 
-  //     return [
-  //       { title: 'Vocabularies', cols: 1, rows: 1 }
-  //     ];
-  //   })
-  // );
-  /** Based on the screen size, switch from standard to one column per row */
-  terms = { title: 'Terms', cols: 1, rows: 2 };
-  // terms = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-  //   map(({ matches }) => {
-  //     if (matches) {
-  //       return [
-  //         { title: 'Terms', cols: 1, rows: 1 }
-  //       ];
-  //     }
+  current_vocab: Vocabulary = {name: '', description:''};
 
-  //     return [
-  //       { title: 'Terms', cols: 1, rows: 2 }
-  //     ];
-  //   })
-  // );
+  private currentVocabSuscription: Subscription = null;
+  private currentVocabObserver: PartialObserver<Vocabulary> = {
+    next: (vocab: Vocabulary) => {
+      this.current_vocab = vocab;
+    },
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+    error: (err: any) => {
+        console.log('The observable got an error notification: ' + err + '.');
+    },
+
+    complete: () => {
+      console.log('The observable got a complete notification.');
+    }
+  };
+
+  constructor(private service: TaxonomyService) {}
+
+  ngOnInit(): void {
+    this.currentVocabSuscription = this.service.currentVocabObservable.subscribe(this.currentVocabObserver);
+  }
+
+  ngOnDestroy(): void {
+    if (this.currentVocabSuscription){
+      this.currentVocabSuscription.unsubscribe();
+    }
+  }
 }
