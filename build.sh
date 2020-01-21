b7792bfbecbcfe2dbdc1f664928bd06f088585e4
Rafael Martinez Estevez <rafael.martinez@upr.edu.cu>
2020-01-21 14:03:34 -0500

    bug in footer import

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-ng build  taxonomy --aot --preserve-symlinks --build-optimizer=true  --verbose  --baseHref https://tocororo.upr.edu.cu/taxonomy --deploy-url https://tocororo.upr.edu.cu/taxonomy/
+echo project name?
+
+read project
+ng build  $project --aot --preserve-symlinks --build-optimizer=true  --verbose  --baseHref https://tocororo.upr.edu.cu/$project --deploy-url https://tocororo.upr.edu.cu/$project/
