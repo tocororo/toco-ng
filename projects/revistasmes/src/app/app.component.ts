fcda2cf487e7c8d6b81951d4325246ef16c1eb64
Edel Abreu <edel.abreu@upr.edu.cu>
2020-03-16 19:25:56 -0400

    add photo in home page, fix footer and update md files

--- projects/revistasmes/src/app/app.component.ts
+++ projects/revistasmes/src/app/app.component.ts
@@ -90,8 +90,8 @@ export class AppComponent {
         this.footerSites.push({ name: "Dirección Nacional de Publicaciones Seriadas", url: "http://www.seriadascubanas.cult.cu/http://www.seriadascubanas.cult.cu/", useRouterLink:false});
         this.footerSites.push({ name: "Red Ciencia", url: "http://www.redciencia.cu/", useRouterLink: false});
 
-        this.footerInformation.push({ name: "Términos de uso", url: "https://www.mes.gob.cu", useRouterLink: false});
-        this.footerInformation.push({ name: "Privacidad", url: "https://sceiba-lab.upr.edu.cu", useRouterLink: false});
+        this.footerInformation.push({ name: "Términos de uso", url: "https://sceiba-lab.upr.edu.cu/page/politicas", useRouterLink: false});
+        this.footerInformation.push({ name: "Privacidad", url: "https://sceiba-lab.upr.edu.cu/page/politicas", useRouterLink: false});
         this.footerInformation.push({ name: "Contacto", url: "/contact", useRouterLink: true});
         this.footerInformation.push({ name: "FAQs", url: "/faq", useRouterLink: true});
     }
