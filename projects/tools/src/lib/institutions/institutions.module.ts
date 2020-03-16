4cc67279df102594f98feb01ccd3e2e4c621b5de
Edel Abreu <edel.abreu@upr.edu.cu>
2020-03-16 19:29:33 -0400

    update

--- projects/tools/src/lib/institutions/institutions.module.ts
+++ projects/tools/src/lib/institutions/institutions.module.ts
@@ -12,11 +12,8 @@ import { ListCountSourcesByTermComponent } from './list-count-sources-by-term/li
 import { SharedModule } from '../shared';
 import { TocoFormsModule } from '../forms';
 import { ExtraInstitutionSelectorComponent } from './extra-institution-selector/extra-institution-selector.component';
-<<<<<<< HEAD
 import { RouterModule } from '@angular/router';
-=======
 import { InstitutionHierarchyViewerComponent } from './institution-hierarchy-viewer/institution-hierarchy-viewer.component';
->>>>>>> 2d6c8dd7911f6e238c32196e25eac22d40e8d49c
 
 
 
