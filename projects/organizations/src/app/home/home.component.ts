24ec3b66245e280248906ab63659ec19c8c20fdc
Rafael Martinez Estevez <rafael.martinez@upr.edu.cu>
2020-08-07 16:09:14 -0400

    home search

--- projects/organizations/src/app/home/home.component.ts
+++ projects/organizations/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
+  query = "";
+  ngOnInit() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  queryChange(event?: string) {
+    console.log(event);
+    this.query = event;
+    this.router.navigate(["search"], {
+      queryParams: {'q': this.query, },
+      queryParamsHandling: "",
+    });
   }
-
 }
