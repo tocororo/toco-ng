2258c383ffa40e303e6d083129f51dea99de2d95
Rafael Martinez Estevez <rafael.martinez@upr.edu.cu>
2020-08-11 10:38:03 -0400

    experimental edit

--- projects/tools/src/lib/forms/experimental/form-field.control.experimental.ts
+++ projects/tools/src/lib/forms/experimental/form-field.control.experimental.ts
@@ -1,7 +1,9 @@
 
-import { Input } from '@angular/core';
+import { Input, OnInit } from '@angular/core';
 
 import { InputContent } from '../input/input.control';
+import { FormFieldControl } from '../form-field.control';
+import { FormControl, FormGroup } from '@angular/forms';
 
 /**
  * A interface that represents the content of a `FormFieldControl_Experimental`. 
@@ -14,11 +16,19 @@ export interface FormFieldContent_Experimental extends InputContent
 /**
  * Represents the base abstract class for a control that is treated as a form field.
  */
-export abstract class FormFieldControl_Experimental
+export abstract class FormFieldControl_Experimental implements OnInit
 {
     /**
      * Input field that contains the content of this class.
      */
     @Input()
     public content: FormFieldContent_Experimental;
+
+    internalControl = new FormControl();
+    
+    ngOnInit()
+    {
+        (this.content.parentFormSection as FormGroup).addControl(this.content.name, this.internalControl);
+      
+    }
 }
