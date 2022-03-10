de74a8e26fe540bf5cc7b8bafa84b85b4f2c2157
luiso <lecc628@gmail.com>
2022-03-09 19:21:10 -0500

    Add the QueryParamKey enum
    The QueryParamKey enum represents the possible query param keys
    that are used in a search request.
    The people-ng project has an example of usage.

--- projects/toco-lib/src/lib/search/public-api.ts
+++ projects/toco-lib/src/lib/search/public-api.ts
@@ -4,7 +4,8 @@
  */
 export * from './search.module';
 
+export * from './aggregations/aggregations.component';
+export * from './query-input/query-input.component';
 export * from './record/record.component';
 export * from './record-list/record-list.component';
-export * from './query-input/query-input.component';
-export * from './aggregations/aggregations.component';
+export * from './search-utils';
