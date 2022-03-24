a647957c2d356efd5edf15f8a890b95423e70a0c
Rafael Martinez Estevez <malayibiri@gmail.com>
2022-03-24 11:32:37 +0100

    remove person module

--- projects/toco-lib/src/public-api.ts
+++ projects/toco-lib/src/public-api.ts
@@ -11,7 +11,6 @@ export * from './lib/filters/public-api';
 export * from './lib/forms/public-api';
 export * from './lib/notification/public-api';
 export * from './lib/organizations/public-api';
-export * from './lib/person/public-api';
 export * from './lib/search/public-api';
 export * from './lib/sources/public-api';
 export * from './lib/statics/public-api';
